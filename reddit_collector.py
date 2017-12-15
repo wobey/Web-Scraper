7ae5de57dafa6cbc51fffa00590eaaaca60f4e03
John Fitzgerald <wobey1@gmail.com>
2017-12-14 17:57:57 -0800

    Refactored and modularized reddit_collector.py

--- reddit_collector.py
+++ reddit_collector.py
@@ -1,8 +1,14 @@
-import requests
+# Author: John Fitzgerald
+# Title: reddit_collector.py
+# Date Modified: 12/14/17
+# Description: Leave script running to parse a subreddit's new posts, and insert them into a database.
+# An email is sent out if the insert fails.
 from bs4 import BeautifulSoup
+from datetime import datetime
+from email.mime import multipart, text
+import requests
 import re
 import post
-from datetime import datetime, timedelta, timezone
 import pytz
 import pyodbc
 import sys
@@ -11,7 +17,6 @@ import textwrap
 import random
 import time
 import smtplib
-from email.mime import multipart, text
 
 
 # get user's input after validating it
@@ -38,7 +43,20 @@ def get_valid_user_input(input_values, user_input_strings, user_input_options):
     return input_values
 
 
+def send_email(domain, pwd, _from, _to, message, title, posted):
+    message.attach(text.MIMEText("title = " + title + "\nposted = " + str(posted), 'plain'))
+    message_text = message.as_string()
+
+    server = smtplib.SMTP_SSL(domain)
+    server.ehlo()
+    server.login(_from, pwd)
+
+    server.sendmail(_from, _to, message_text)
+    server.quit()
+
+
 def main():
+    comment_url = "r/Seattle/comments/"
     please_str = "Please enter a valid "
     user_input_options = [r"^https://reddit.com/r/([0-9A-Z]+)", r"(.+)", r"(.+)", r"(.+)", r"(.+)"]
     user_input_strings = [please_str + "subreddit url: ", please_str + "db url: ", please_str + "email domain: ",
@@ -56,14 +74,11 @@ def main():
     server.login(email_address_from, email_pwd)
     server.quit()
 
+    # create email (sent if error is detected)
     message = multipart.MIMEMultipart()
     message['From'] = email_address_from
     message['To'] = email_address_to
     message['Subject'] = "ALERT:  Reddit Collector"
-    message.attach(text.MIMEText("empty", 'plain'))
-    message_text = message.as_string()
-
-    comment_url = "r/Seattle/comments/"
 
     session = requests.Session()
     headers = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64)"
@@ -72,9 +87,6 @@ def main():
                "Accept":"text/html,application/xhtml+xml,application/xml"
                         ";q=0.9,image/webp,image/apng,*/*;q=0.8"}
 
-    # subreddit_url = "https://reddit.com/r/Seattle/new"
-    # url = "https://weather.com/weather/hourbyhour/l/98203:4:US"
-
     minutes = 60.0
     seconds = 60.0
     jitter = 0.17
@@ -82,11 +94,6 @@ def main():
     request_rate_large = float(minutes * (1.0 + jitter))
     actual_request_rate = seconds * random.uniform(request_rate_small, request_rate_large)
     duplicate_factor = 0.5
-
-    # print("Request rate small = " + str(request_rate_small))
-    # print("Request rate large = " + str(request_rate_large))
-    # print("Actual request rate = " + str(actual_request_rate))
-
     duplicate_count = 0
     insert_count = 0
 
@@ -96,39 +103,34 @@ def main():
         print("\n\t\tTOTAL RUNTIME = " + str(time.time() - total_run_start_time) + "\n")
 
         start_time = time.time()
+        posts = post.Posts(subreddit_url)
+        duplicate_check = False
 
+        # get HTML request
         req = session.get(subreddit_url, headers=headers)
-
         bs_obj = BeautifulSoup(req.text, "lxml")
-        # print(bs_obj.prettify())
-
-        Posts = post.Posts(subreddit_url)
-
         div_classes = bs_obj.find_all('div', attrs={'class': re.compile(r'thing$')})
-
-        # for div in div_classes:
-        #     print(div)
-        # print()
 
         title = date_time = user = url = "null"
 
         for i in range(0, len(div_classes)):
             line = str(div_classes[i])
 
+            # pull relevant lines from HTML
             title_match = re.search(r'tabindex="1">(.+?)</a>', line)
-            # title_match = re.search(r'>(.+?)</a>', str(titles[i]))
             time_match = re.search(r'data-timestamp="(.+?)"', line)
-            # time_match = re.search(r'"datetime="(.+?)"', str(times[i]))
             user_match = re.search(r'data-author="(.+?)"', line)
             url_match = re.search(r'href="(.+?)" rel="', line)
 
+            # parse lines looking for regular expression matches
             if title_match:
                 title = title_match.group(1)
             if time_match:
                 date_time = time_match.group(1)
                 date_time = datetime.utcfromtimestamp(int(date_time[:-3]))  # convert to UTC date time
                 tz = pytz.timezone('America/Los_Angeles')
-                date_time = pytz.utc.localize(date_time, is_dst=None).astimezone(tz).replace(tzinfo=None)  # convert to Seattle time
+                # convert to Seattle time
+                date_time = pytz.utc.localize(date_time, is_dst=None).astimezone(tz).replace(tzinfo=None)
             if user_match:
                 user = user_match.group(1)
             if url_match:
@@ -136,81 +138,66 @@ def main():
                 if comment_url in url:
                     url = "https://reddit.com" + url
 
-            Posts.add(date_time, title, user, url)
+            posts.add(date_time, title, user, url)
             title = date_time = user = url = "null"
 
-        # for count, (key, value) in enumerate(Posts.posts.items()):
-        #     print(str(count + 1) + ") " + key)
-        #     print("\t" + str(value.date_time))
-        #     print("\t" + value.user)
-        #     print("\t" + value.url)
-        #     print("\t" + value.title)
-
-        server = db_url
+        # initialize connection to database using installed ODBC SQL Server driver
         database = 'Collector'
         username = 'Wobey'
         password = db_pwd
         cnxn = pyodbc.connect(
-            'DRIVER={ODBC Driver 13 for SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
+            'DRIVER={ODBC Driver 13 for SQL Server};SERVER=' + db_url + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password)
         cursor = cnxn.cursor()
 
-        duplicate = False
-
-        for count, (key, value) in enumerate(Posts.posts.items()):
-            # determine if duplicate exists (based on datetime added and username)
-
+        # insert posts into database
+        for count, (key, value) in enumerate(posts.posts.items()):
             sql_exists = textwrap.dedent("""SELECT * FROM Collector.guest.Posts WHERE datetime_posted = (?) and username = (?);""")
             cursor.execute(sql_exists, value.date_time, value.user)
-            # print("\n[scraped HTML] = " + str(value.date_time))
             row = cursor.fetchall()
 
             # because r/Seattle/new orders postings by time, the first duplicate should trigger a break
             if len(row) >= 1:
                 duplicate_count += 1
                 print("[duplicate " + str(duplicate_count) + "] = " + str(row))
+
                 # sleep for a quarter of the time to attempt a faster request
                 elapsed = time.time() - start_time
                 sleep_length = duplicate_factor * (actual_request_rate - elapsed)
+
                 print("[sleep] = " + str(sleep_length))
                 time.sleep(sleep_length)
-
+                duplicate_check = True
                 break
             else:
                 sql_insert = textwrap.dedent("""
                 INSERT INTO Collector.guest.Posts(datetime_added, datetime_posted, username, url_path, title)
                     VALUES (?, ?, ?, ?, ?);""")
 
-                cursor.execute(sql_insert, datetime.now(), value.date_time, value.user, value.url, value.title)
-                # cursor.execute(sql_insert, datetime.now(), value.date_time, value.temp, value.wind, value.phrase)
+                # trigger email if the insert was unsuccessful
+                try:
+                    cursor.execute(sql_insert, datetime.now(), value.date_time, value.user, value.url, value.title)
+                except pyodbc.DatabaseError as e:
+                    send_email(email_domain, email_pwd, email_address_from, email_address_to, message, value.title, value.date_time)
+                    raise e("Insert failed = " + str(value.title))
 
                 insert_count += 1
                 print("[insert " + str(insert_count) + "] = " + str(datetime.now()))
                 print("\t" + value.title)
-                # print("\t" + str(value.date_time))
-                # print("\t" + value.user)
-                # print("\t" + value.url)
-                # print("\t" + value.title)
 
+            # trigger email if the insert was unsuccessful
             try:
                 cnxn.commit()
             except pyodbc.DatabaseError as e:
-                print("*** DatabaseError: " + str(e))
-                server = smtplib.SMTP_SSL(email_domain)
-                server.ehlo()
-                server.login(email_address_from, email_pwd)
-
-                server.sendmail(email_address_from, email_address_to, message_text)
-                server.quit()
-                pass
+                send_email(email_domain, email_pwd, email_address_from, email_address_to, message, value.title, value.date_time)
+                raise e('Insert failed.')
 
         elapsed = time.time() - start_time
-        if elapsed < actual_request_rate:
+        if elapsed < actual_request_rate and duplicate_check is False:
             print("[sleep] = " + str(actual_request_rate - elapsed))
             time.sleep(actual_request_rate - elapsed)
             actual_request_rate = seconds * random.uniform(request_rate_small, request_rate_large)
 
-        # TODO free up Posts depending on memory useage
-        del Posts
+        del posts
 
 
 if __name__ == "__main__":
